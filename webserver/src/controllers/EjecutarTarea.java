f7d0fbc20ea2117d2d251c17528e464c0478b8a0
wizzac <sebycuk@yahoo.com.ar>
2018-10-31 04:40:57 -0300

    commit de las 4:47am

--- webserver/src/controllers/EjecutarTarea.java
+++ webserver/src/controllers/EjecutarTarea.java
@@ -10,46 +10,32 @@ import java.util.Map;
 import java.util.concurrent.*;
 
 
-public class EjecutarTarea implements Callable {
-    //public class EjecutarTarea implements Callable {
-    private int backendServerPort;
-    private JSONObject request;
-    private Map response;
-    private int user;
-    private Respuesta r;
-    //    private Respuesta r= new Respuesta();
-    private Double conversionRate;
-    private Double total=0d;
-    private ExecutorService servicio = Executors.newFixedThreadPool(100);
-    private Map requestProperties;
+//public class EjecutarTarea implements Callable {
+public class EjecutarTarea {
+    private static Map response;
+    private static int user;
+    private static Respuesta r;
+    private static Double conversionRate;
+    private static Double total=0d;
+    private static ExecutorService pool = Executors.newFixedThreadPool(10);
+    private static Map requestProperties;
 
 
-    public EjecutarTarea(Integer backendServerPort, JSONObject request,int user,Double conversionRate,Map requestProperties){
-        this.backendServerPort=backendServerPort;
-        this.request=request;
+    public EjecutarTarea(int user,Double conversionRate,Map requestProperties){
         this.user=user;
         this.r=r;
         this.conversionRate=conversionRate;
         this.requestProperties=requestProperties;
     }
 
-    public Respuesta call() {
-//    public void run() {
+    //    @Override
+    public static Respuesta call() {
         JSONObject finalResponse = new JSONObject();
-        Double totalAmount = 0D;
-//        Map requestProperties = new LinkedHashMap();
-//        requestProperties.put("baseUrl", "http://localhost:" + backendServerPort);
-//        requestProperties.put("method", "GET");
-//        requestProperties.put("body", new LinkedHashMap());
-//        requestProperties.put("headers", new LinkedHashMap());
-//        requestProperties.put("socketTimeout", 2000);
-//        requestProperties.put("connectionTimeout",300);
         final String sold="/soldItems/";
         final String item="/items/";
         Integer responseStatus;
         CompletableFuture future=new CompletableFuture<>();
-        Future<Double> promesa;
-
+        CompletableFuture futureNotify=new CompletableFuture<>();
         try {
             for (int attempt = 0; attempt < 3; attempt++) {
                 finalResponse.put("sellerId", user);
@@ -61,67 +47,68 @@ public class EjecutarTarea implements Callable {
                     continue;
                 }
             }
-                Map responseBody = (Map) response.get("body");
-                if (!"seller".equals(responseBody.get("user_type"))) {
-                    setServiceFailedResponse("user is not seller");
-                    return r;
-                } else {
-                    requestProperties.put("uriWithQueryString",sold + user);
-                    for (int attempt2 = 0; attempt2 < 3; attempt2++) {
-                        response = HttpClient.executeRequest(requestProperties);
-                        responseStatus = (Integer) response.get("status");
-                        if (responseStatus > 201) {
-                            setServiceFailedResponse("could not get soldItems for user " + user);
-                            continue;
-                        }else{
-                            break;
-                        }
-                    }
-                    List soldItems = (List) response.get("body");
-
-                    for (int i = 0; i < soldItems.size(); i++) {
-                        Map itemJson = (Map) soldItems.get(i);
-                        Long itemId = (Long) itemJson.get("id");
-                        requestProperties.put("uriWithQueryString",item + itemId.toString());
-                        future=CompletableFuture.runAsync(()->{
-                            Double resParcial=0d;
-                            for (int attempt3 = 0; attempt3 < 3; attempt3++) {
-                                response = HttpClient.executeRequest(requestProperties);
-                                Integer status = (Integer) response.get("status");
-                                if (status > 201) {
-                                    continue;
-                                }else{
-                                    Map itemInfo = (Map) response.get("body");
-                                    resParcial=(Double) itemInfo.get("price");
-                                    totalPorTread(resParcial);
-                                    break;
-                                }
-                            }
-                        });
+            Map responseBody = (Map) response.get("body");
+            if (!"seller".equals(responseBody.get("user_type"))) {
+                setServiceFailedResponse("user is not seller");
+                return r;
+            } else {
+                requestProperties.put("uriWithQueryString",sold + user);
+                for (int attempt2 = 0; attempt2 < 3; attempt2++) {
+                    response = HttpClient.executeRequest(requestProperties);
+                    responseStatus = (Integer) response.get("status");
+                    if (responseStatus > 201) {
+                        setServiceFailedResponse("could not get soldItems for user " + user);
+                        continue;
+                    }else{
+                        break;
                     }
                 }
+                List soldItems = (List) response.get("body");
+                for (int i = 0; i < soldItems.size(); i++) {
+                    Map itemJson = (Map) soldItems.get(i);
+                    Long itemId = (Long) itemJson.get("id");
+                    requestProperties.put("uriWithQueryString",item + itemId.toString());
+                    future=CompletableFuture.runAsync(()->{
+                        Double resParcial=0d;
+                        for (int attempt3 = 0; attempt3 < 3; attempt3++) {
+                            response = HttpClient.executeRequest(requestProperties);
+                            Integer status = (Integer) response.get("status");
+                            if (status > 201) {
+                                continue;
+                            }else{
+                                Map itemInfo = (Map) response.get("body");
+                                resParcial=(Double) itemInfo.get("price");
+                                totalPorTread(resParcial);
+                                break;
+                            }
+                        }
+                    });
+                }
+            }
 
             future.get();
-            Double totalAmountUSD = this.total / conversionRate;
-        //    notifyResponse(requestProperties,user, totalAmountUSD);
-            finalResponse.put("totalAmount", this.total);
+            Double totalAmountUSD = total / conversionRate;
+            futureNotify=CompletableFuture.runAsync(()-> {
+                notifyResponse(requestProperties, user, totalAmountUSD);
+            });
+            futureNotify.get();
+            finalResponse.put("totalAmount", total);
             finalResponse.put("totalAmountUSD", totalAmountUSD);
-            this.r=new Respuesta(finalResponse.toString(),200);
+            r=new Respuesta(finalResponse.toString(),200);
         } catch (Exception e) {
             System.out.println("Exception " + e);
         }
-        servicio.shutdown();
         return r;
     }
 
-    private void notifyResponse(Map requestProperties,Integer userToNotify, Double amountToNotify){
+    private static void notifyResponse(Map requestProperties,Integer userToNotify, Double amountToNotify){
         Map bodyToNotify = new LinkedHashMap();
         bodyToNotify.put("id", userToNotify);
         bodyToNotify.put("amount", amountToNotify);
         requestProperties.put("uriWithQueryString", "/notifications");
         requestProperties.put("body", bodyToNotify);
         try{
-            for (int attempt = 0; attempt < 5; attempt++) {
+            for (int attempt = 0; attempt < 3; attempt++) {
                 Map notoificationResponse = HttpClient.executeRequest(requestProperties);
                 Integer responseStatus = (Integer) notoificationResponse.get("status");
                 if (responseStatus > 201) {
@@ -134,11 +121,10 @@ public class EjecutarTarea implements Callable {
         }
         catch(Exception e){
             setServiceFailedResponse("could not notify result");
-            return;
         }
     }
 
-    private void setServiceFailedResponse(String message){
+    private static void setServiceFailedResponse(String message){
         JSONObject finalResp=new JSONObject();
         try{
             finalResp.put("message",message);
@@ -149,17 +135,19 @@ public class EjecutarTarea implements Callable {
         return;
     }
 
-    private void totalPorTread(Double suma){
+    private static void totalPorTread(Double suma){
         synchronized (total) {
             total += suma;
         }
     }
 
-    public Future<Respuesta> getCall(){
-        return servicio.submit(()->{
+    public static Future<Respuesta> getCall(){
+        return pool.submit(()->{
             return call();
         });
     }
+
+
 
 
 }
--- webserver/src/controllers/SalesController.java
+++ webserver/src/controllers/SalesController.java
@@ -1,10 +1,8 @@
 package controllers;
 
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import utils.HttpClient;
@@ -15,17 +13,19 @@ public class SalesController extends MainController {
     private int backendServerPort = 8888;
     private Double conversionRate = CurrencyController.currency;
     private Respuesta respuesta;
-//    private eventListener mListener;
+    private ExecutorService pool=Executors.newCachedThreadPool();
 
-//    public void registrarEvento(eventListener mListener) {
-//        this.mListener = mListener;
-//    }
+    //    private eventListener mListener;
+    //    public void registrarEvento(eventListener mListener) {
+    //        this.mListener = mListener;
+    //    }
 
 
     public void index() {
 
-        JSONObject finalResponse = new JSONObject();
-        Double totalAmount = 0D;
+
+        Respuesta respuesta=null;
+        System.out.println("------------------Controler----------------");
         Map requestProperties = new LinkedHashMap();
         final String curr = "/currency_conversions?from=USD&to=ARS";
         requestProperties.put("baseUrl", "http://localhost:" + backendServerPort);
@@ -33,80 +33,66 @@ public class SalesController extends MainController {
         requestProperties.put("body", new LinkedHashMap());
         requestProperties.put("headers", new LinkedHashMap());
         requestProperties.put("socketTimeout", 2000);
-        requestProperties.put("connectionTimeout", 300);
+        requestProperties.put("connectionTimeout", 400);
         requestProperties.put("uriWithQueryString", curr);
-        Integer responseStatus;
-        Future<Respuesta> respuestaFuture=null;
-        CompletableFuture future = new CompletableFuture<>();
         Integer userId = 0;
         try {
-
             JSONObject parameters = request.getJSONObject("parameters");
             JSONArray userIdArray = parameters.getJSONArray("userId");
             userId = Integer.valueOf(userIdArray.get(0).toString());
-
         } catch (Exception e) {
             setServiceFailedResponse("No id Provided");
         }
-        if(this.respuesta==null){
-            EjecutarTarea tarea = new EjecutarTarea(backendServerPort, request, userId, conversionRate,requestProperties);
-            respuestaFuture=tarea.getCall();
-
-//            future = CompletableFuture.runAsync(() -> {
-//                Future<Respuesta> promesa;
-//                try {
-//                    promesa = servicio.submit(tarea);
-//                    setRespuesta(promesa.get());
-//                } catch (Exception e) {
-//                    setServiceFailedResponse("could not get user information");
-//                }
-//            });
-//
-//            try {
-//                future.get();
-//            } catch (Exception e) {
-//                System.out.println(e.getMessage());
+        if (respuesta == null) {
+            EjecutarTarea tarea = new EjecutarTarea(userId, conversionRate, requestProperties);
+            MyRunnable worker=new MyRunnable(tarea);
+            try {
+                Future<Respuesta> fut=pool.submit(worker);
+                setRespuesta(fut.get());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                setServiceFailedResponse("Too much time ");
+            }
+        }else {
+            setServiceFailedResponse("No id Provided");
         }
-//        }
-        try {
-            setRespuesta(respuestaFuture.get());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        showResponse();
+    }
 
+    private void setServiceFailedResponse(String message) {
+        this.respuesta = new Respuesta("{'message':'" + message + "'}", 200);
+        return;
+    }
+
+    private void setRespuesta(Respuesta r) {
+        this.respuesta = r;
+        return;
+    }
+
+    private void showResponse(){
         setResponseBody(respuesta.getMsj());
         setResponseStatus(respuesta.getStatus());
         setResponseHeader("content-type", "application/json");
     }
 
-    private void setServiceFailedResponse(String message){
-        this.respuesta=new Respuesta("{'message':'" + message + "'}",200);
-        return;
-    }
+    class MyRunnable implements Callable {
 
-    private void setRespuesta(Respuesta r){
-        this.respuesta=r;
-        return;
-    }
+        private EjecutarTarea tarea;
+        MyRunnable(EjecutarTarea tarea) {
+            this.tarea=tarea;
+        }
 
-//
-//    interface eventListener {
-//        void reponse();
-//    }
-//
-//    public void reponse() {
-//        setResponseBody(respuesta.getMsj());
-//        setResponseStatus(respuesta.getStatus());
-//        setResponseHeader("content-type", "application/json");
-//    }
-//
-//
-//    class Responsor implements eventListener {
-//        @Override
-//        public void reponse() {
-//            setResponseBody(respuesta.getMsj());
-//            setResponseStatus(respuesta.getStatus());
-//            setResponseHeader("content-type", "application/json");
-//        }
-//    }
+        @Override
+        public Respuesta call() {
+            Respuesta res=new Respuesta();
+            try {
+                Future<Respuesta> respuestaFuture = tarea.getCall();
+                res=respuestaFuture.get();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+
+            }
+            return res;
+        }
+    }
 }
